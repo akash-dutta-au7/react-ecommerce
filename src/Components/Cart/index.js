8cfe3020f8f662e960889715ef20a38e72cceaa0
Akash Dutta <dutta.akash1993official@gmail.com>
2020-09-13 22:33:39 +0530

    cart dropdown, item added to cart

--- src/Components/Cart/index.js
+++ src/Components/Cart/index.js
@@ -1,14 +1,23 @@
 import React from "react";
 import "./index.css";
+import { connect } from "react-redux";
+import CartItem from "../CartItem";
 import Button from "../Button";
 
-const Cart = () => {
+const Cart = ({ cartItems }) => {
   return (
     <div className="cart">
-      <div className="cart-item"> </div>
-      <Button value="CHECKOUT" isCartButton/>
+      <div className="cart-item">
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
+      </div>
+      <Button value="CHECKOUT" isCartButton />
     </div>
   );
 };
 
-export default Cart;
+const mapStateToProps = ({cartReducer: {cartItems}}) => ({
+  cartItems
+});
+export default connect(mapStateToProps)(Cart);
--- src/Redux/Utils/cart.utils.js
+++ src/Redux/Utils/cart.utils.js
@@ -1,11 +1,11 @@
 export const addToCart = (cartItems, cartItemToAdd) => {
    const existingCartItem = cartItems.find(
-    cartItem => cartItem.id == cartItemToAdd.id
+    cartItem => cartItem.id === cartItemToAdd.id
   );
 
   if (existingCartItem) {
     return cartItems.map((cartItem) =>
-      cartItem.id == cartItemToAdd.id
+      cartItem.id === cartItemToAdd.id
         ? { ...cartItem, quantity: cartItem.quantity + 1 }
         : cartItem
     );
